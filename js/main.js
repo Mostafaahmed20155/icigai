9a944e76a53dc42f7fb6203801c37516b92127c3
Mostafa Saad <mostafa.saad127@yahoo.com>
2020-08-01 21:15:16 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//PARALLAX EFFECT
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+//STICKY NAV BAR
+window.addEventListener("scroll", () => {
+    var navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle("sticky", window.scrollY > 0)
+})
+
+//FACEBOOK
+function fbIconSelected() {
+    document.querySelector('.fb-icon').src = "./img/Icon awesome-facebook-f-selected.svg";
+}
+function fbIconNotSelected() {
+    document.querySelector('.fb-icon').src = "./img/Icon awesome-facebook-f.svg";
+}
+//LINKED IN
+function linkedinIconSelected() {
+    document.querySelector('.linkedin-icon').src = "./img/Icon awesome-linkedin-in-selected.svg";
+}
+function linkedinIconNotSelected() {
+    document.querySelector('.linkedin-icon').src = "./img/Icon awesome-linkedin-in.svg";
+}
+//INSTGRAM
+function instIconSelected() {
+    document.querySelector('.inst-icon').src = "./img/Icon awesome-instagram-selected.svg";
+}
+function instIconNotSelected() {
+    document.querySelector('.inst-icon').src = "./img/Icon awesome-instagram.svg";
+}
+//INSTGRAM
+function twtIconSelected() {
+    document.querySelector('.twt-icon').src = "./img/Icon awesome-twitter-selected.svg";
+}
+function twtIconNotSelected() {
+    document.querySelector('.twt-icon').src = "./img/Icon awesome-twitter.svg";
+}
+
+//PRODUCTS SECTION
+const prod1 = document.getElementById('prod1');
+const prod2 = document.getElementById('prod2');
+const prod3 = document.getElementById('prod3');
+const prod4 = document.getElementById('prod4');
+const prod5 = document.getElementById('prod5');
+const prod6 = document.getElementById('prod6');
+const prodTitle = document.querySelector(".product-title");
+const prodDetails = document.querySelector(".product-details");
+
